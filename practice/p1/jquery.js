869d7ffba08fafcb3f25e7ca9567b38b54e541bf
EC2 Default User <ec2-user@ip-172-31-19-205.us-east-2.compute.internal>
2020-02-17 08:05:54 +0000

    first commit

---
+++ practice/p1/jquery.js
@@ -0,0 +1,12 @@
+ $(document).ready(function(){
+
+            var styles = {
+                backgroundColor : "purple",
+            };
+            
+            $("#div").css(styles);
+            
+            $("#highlight").css("border", "200px solid black");
+            
+                
+})
